bc8e99734333afac443a8ecf41969861e24bef59
boom <boomtanut@gmail.com>
2023-10-05 23:42:55 +0700

    assignment-8

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import Image from 'next/image'
-import styles from './page.module.css'
 import Banner from '@/components/Banner'
-import ProductCard  from '@/components/ProductCard'
-import CardPanel from '@/components/CardPanel'
+import { PromoteCard } from '@/components/PromoteCard'
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Banner/> 
+      <PromoteCard/>
         {/* <ProductCard hospitalName='Chulalongkorn Hospital'imgSrc='/img/chula.jpg'/>
         <ProductCard hospitalName='Rajavithi Hospital'imgSrc='/img/rajavithi.jpg'/>
         <ProductCard hospitalName='Thammasat University Hospital'imgSrc='/img/thammasat.jpg'/> */}
-    </div>
+    </main>
   )
 }
